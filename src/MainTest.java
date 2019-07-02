4fa2e0b6043c394648d7e9561d4678fa4160d428
Zhukova.A <azhukova@stream.ru>
2019-07-02 10:37:58 +0300

    test

---
+++ src/MainTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+public class MainTest extends CoreTestCase
+
+{
+
+    @Test
+  public void myFirstTest() {
+
+
+
+    }
+
+    @Test
+    public void mySecondTest() {
+
+       
+
+    }
+
+
+
+
+
+
+
+
+}
