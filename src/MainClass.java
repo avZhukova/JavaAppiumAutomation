e95d4b42ae5887283185425091b0fc43cca082a8
Zhukova.A <azhukova@stream.ru>
2019-07-02 11:38:18 +0300

    test 1

---
+++ src/MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass {
+
+
+public int getLocalNumber()
+{
+
+   return 14;
+
+}
+
+
+}
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,28 +1,24 @@
 import org.junit.Test;
 
-public class MainTest extends CoreTestCase
+public class MainTest extends MainClass
 
 {
 
     @Test
-  public void myFirstTest() {
+  public void testGetLocalNumber() {
 
+
+        if (getLocalNumber() == 14) {
+
+            System.out.println("Успех");
+
+        } else {
+            System.out.println("Число не равно 14");
+
+        }
 
 
     }
-
-    @Test
-    public void mySecondTest() {
-
-       
-
-    }
-
-
-
-
-
-
 
 
 }
